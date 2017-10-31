5c298792694acbd23372ddb58a74bcf5bc759df3
Kevin.Allain <kevallain@gmail.com>
2017-10-31 15:17:00 +0000

    Put a line, the idea is to do the same around the cylinder in order to indicate probablilities of directions of the catastrophy.

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ var windowHalfY = window.innerHeight / 2;
 var splineHelperObjects = [], splineOutline;
 var splinePointsLength = 4; var positions = []; var options;
 var geometry = new THREE.BoxGeometry( 20, 20, 20 ); var transformControl;
-var ARC_SEGMENTS = 200; var splineMesh; var splines = {};
-var params = { uniform: true, tension: 0.5, centripetal: true, chordal: true, addPoint: addPoint, removePoint: removePoint, exportSpline: exportSpline };
+// var ARC_SEGMENTS = 200; var splineMesh; var splines = {};
+// var params = { uniform: true, tension: 0.5, centripetal: true, chordal: true, addPoint: addPoint, removePoint: removePoint, exportSpline: exportSpline };
 
 var hiding;
 
@@ -52,61 +52,6 @@ function init() {
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0xffffff );
 
-	// Gui
-	var gui = new dat.GUI();
-	gui.add( params, 'uniform' );
-	gui.add( params, 'tension', 0, 1 ).step( 0.01 ).onChange( function( value ) {
-		splines.uniform.tension = value; updateSplineOutline();
-	});
-	gui.add( params, 'centripetal' ); gui.add( params, 'chordal' );
-	gui.add( params, 'addPoint' ); gui.add( params, 'removePoint' );
-	gui.add( params, 'exportSpline' );
-	gui.open();
-
-	/*******
-	 * Curves and boxes
-	 *********/
-	for ( var i = 0; i < splinePointsLength; i ++ ) { addSplineObject( positions[ i ] ); }
-	positions = [];
-
-	for ( var i = 0; i < splinePointsLength; i ++ ) { positions.push( splineHelperObjects[ i ].position ); }
-	var geometry = new THREE.Geometry();
-	for ( var i = 0; i < ARC_SEGMENTS; i ++ ) { geometry.vertices.push( new THREE.Vector3() ); }
-
-	var curve = new THREE.CatmullRomCurve3( positions );
-	curve.type = 'catmullrom';
-	curve.mesh = new THREE.Line( geometry.clone(), new THREE.LineBasicMaterial( {
-		color: 0xff0000, opacity: 0.35, linewidth: 2
-		} ) );
-	curve.mesh.castShadow = true; splines.uniform = curve;
-
-	curve = new THREE.CatmullRomCurve3( positions );
-	curve.type = 'centripetal';
-	curve.mesh = new THREE.Line( geometry.clone(), new THREE.LineBasicMaterial( {
-		color: 0x00ff00, opacity: 0.35, linewidth: 2
-		} ) );
-	curve.mesh.castShadow = true;
-	splines.centripetal = curve;
-
-	curve = new THREE.CatmullRomCurve3( positions );
-	curve.type = 'chordal';
-	curve.mesh = new THREE.Line( geometry.clone(), new THREE.LineBasicMaterial( {
-		color: 0x0000ff, opacity: 0.35, linewidth: 2
-		} ) );
-	curve.mesh.castShadow = true;
-	splines.chordal = curve;
-
-	for ( var k in splines ) {
-		var spline = splines[ k ]; scene.add( spline.mesh );
-	}
-
-	load( [ new THREE.Vector3( 0, 500, 0 ),
-			new THREE.Vector3( -53.56300074753207, 171.49711742836848, -14.495472686253045 ),
-			new THREE.Vector3( -91.40118730204415, 176.4306956436485, -6.958271935582161 ),
-			new THREE.Vector3( -383.785318791128, 491.1365363371675, 47.869296953772746 ) ] );
-	// -- 
-
-
 	// camera
 	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );	
 	camera.position.z = 200;
@@ -121,6 +66,7 @@ function init() {
 	console.log("Post calculatePosColor: "); console.log(dataLand);		
 	drawGroundThree();
 	drawPlane();
+	drawLine3D();
 	// --
 
 	// Rendered
@@ -139,7 +85,6 @@ function init() {
 	controlsCylinder.noPan = false;
 	controlsCylinder.staticMoving = true;
 	controlsCylinder.dynamicDampingFactor = 0.3;
-
 
 	// transformControl
 	transformControl = new THREE.TransformControls( camera, renderer.domElement );
@@ -189,17 +134,14 @@ function init() {
 	dragControlsCylinder.addEventListener( 'dragend', function ( event ) { 
 		controlsCylinder.enabled = true; 
 		controls.enabled=true;
-
 		// Movement done, let's recalculate the colors
 		calculatePosColor();
 		console.log("dataLand: ");console.log(dataLand);
 		colorGroundThree();
 	} );
 
-
 	console.log("wireframe");console.log(wireframe);
 	console.log("cylinder");console.log(cylinder);
-
 }
 
 function onWindowResize() {
@@ -208,11 +150,6 @@ function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function onDocumentMouseMove(event) {
-	// mouseX = ( event.clientX - windowHalfX ) * 10;
-	// mouseY = ( event.clientY - windowHalfY ) * 10;
 }
 
 //
@@ -265,6 +202,37 @@ function drawGroundThree(){
 	colorGroundThree();
 }
 
+function drawLine3D(){
+
+	var geometry3 = new THREE.Geometry(),
+		points =  [new THREE.Vector3( 0 , 100, 0 ),
+			new THREE.Vector3( 40 , 100, 0 ),
+			new THREE.Vector3( 80 , 100, 0),
+			new THREE.Vector3( 120 , 100, 0)
+			],
+		colors3 = [];
+
+	for ( i = 0; i < points.length; i ++ ) {
+		geometry3.vertices.push( points[ i ] );
+		colors3[ i ] = new THREE.Color( 0xffffff );
+		colors3[ i ].setHSL( 0, 1.0, 0.5 );
+	}
+
+	geometry3.colors = colors3;
+	// lines
+	material = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 1, linewidth: 3, vertexColors: THREE.VertexColors } );
+	var line, p, scale = 0.3, d = 225;
+	line = new THREE.Line(geometry3, material );
+	line.scale.x = line.scale.y = line.scale.z =  scale*1.5;
+	line.position.x = cylinder.position.x;
+	line.position.y = 0;
+	line.position.z = cylinder.position.z;
+	console.log("geometry3: ");console.log(geometry3);
+	console.log("line: ");console.log(line);
+	scene.add( line );
+
+}
+
 function colorGroundThree(){
 	for (var i =0; i < dataLand.states.length; i++) {
 		var curState = dataLand.states[i];
@@ -286,13 +254,10 @@ function colorGroundThree(){
 
 		// Types of maximum risk = unique hue (from red to green, we will keep blue for sea I guess)
 		var colorRisk = new THREE.Color("hsl(0,0%,80%)");
-		// Level of risk = higher saturation
-		console.log("valueMaxRisk: "+valueMaxRisk);
-
 		var saturationTab = 100 * valueMaxRisk / (curState.distanceToCatastrophy*10);
-		console.log("saturationTab: "+saturationTab);
+		// console.log("valueMaxRisk: "+valueMaxRisk); console.log("saturationTab: "+saturationTab);
 
-	    switch(typeMaxRisk){
+	    switch(typeMaxRisk) {
 	    	case "alpha":
 	    		colorRisk = new THREE.Color("hsl(0.1,"+ (1 + Math.round(saturationTab*90) ) +"% ,60%)");
     		break;
@@ -327,8 +292,8 @@ function drawCylinder(){
 	wireframe = new THREE.LineSegments( geo, mat );
     wireframe.position.x = 40;
     wireframe.position.z = 60;
-    wireframe.position.y = 20;	
-	scene.add( wireframe );	
+    wireframe.position.y = 20;
+	scene.add( wireframe );
 
 	cylinder.children = wireframe;
     console.log("cylinder: "); console.log(cylinder);
@@ -348,11 +313,11 @@ function calculatePosColor(){
 		var distLandCylinder = Math.sqrt( distXSquare + distZSquare );
 
 		// Added for results to be usable
-		var localCalculRiskAlpha = (dataLand.states[i].paramA 
+		var localCalculRiskAlpha = (dataLand.states[i].paramA
 			* cylinder.paramsCatastrophy.catastrophyParamA) ;
 		var localCalculRiskBeta = (dataLand.states[i].paramB 
 			* cylinder.paramsCatastrophy.catastrophyParamB) ;
-		var localCalculRiskGamma = (dataLand.states[i].paramC 
+		var localCalculRiskGamma = (dataLand.states[i].paramC
 			* cylinder.paramsCatastrophy.catastrophyParamC) ;
 
 		if (maximumRiskGlobal == 0.0){ maximumRiskGlobal = localCalculRiskGamma; }
@@ -391,11 +356,6 @@ function render() {
 		ry = Math.sin( time * 0.3 ) * 0.5,
 		rz = Math.sin( time * 0.2 ) * 0.5;
 
-	// Render code from the webgl_geometry_splines
-	splines.uniform.mesh.visible = params.uniform;
-	splines.centripetal.mesh.visible = params.centripetal;
-	splines.chordal.mesh.visible = params.chordal;
-
 	// RAY CASTER
 	// update the picking ray with the camera and mouse position
 	raycaster.setFromCamera( mouse, camera );
@@ -426,102 +386,9 @@ function render() {
 	wireframe.position.y = cylinder.position.y;
 	wireframe.position.z = cylinder.position.z;
 
+
+
 	renderer.render( scene, camera );
-}
-
-function addPoint() {
-	splinePointsLength ++;
-	positions.push( addSplineObject().position );
-	updateSplineOutline();
-}
-
-function removePoint() {
-	if ( splinePointsLength <= 4 ) {
-		return;
-	}
-	splinePointsLength --;
-	positions.pop();
-	scene.remove( splineHelperObjects.pop() );
-	updateSplineOutline();
-}
-
-function updateSplineOutline() {
-	for ( var k in splines ) {
-		var spline = splines[ k ];
-		splineMesh = spline.mesh;
-		for ( var i = 0; i < ARC_SEGMENTS; i ++ ) {
-			var p = splineMesh.geometry.vertices[ i ];
-			p.copy( spline.getPoint( i /  ( ARC_SEGMENTS - 1 ) ) );
-		}
-		splineMesh.geometry.verticesNeedUpdate = true;
-	}
-}
-
-function exportSpline() {
-	var strplace = [];
-	for ( var i = 0; i < splinePointsLength; i ++ ) {
-		var p = splineHelperObjects[ i ].position;
-		strplace.push( 'new THREE.Vector3({0}, {1}, {2})'.format( p.x, p.y, p.z ) )
-	}
-	console.log( strplace.join( ',\n' ) );
-	var code = '[' + ( strplace.join( ',\n\t' ) ) + ']';
-	prompt( 'copy and paste code', code );
-}
-
-function addSplineObject( position ) {
-	var material = new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } );
-	var object = new THREE.Mesh( geometry, material );
-	if ( position ) {
-		object.position.copy( position );
-	} else {
-		object.position.x = Math.random() * 1000 - 500;
-		object.position.y = Math.random() * 600;
-		object.position.z = Math.random() * 800 - 400;
-	}
-
-	object.castShadow = true;
-	object.receiveShadow = true;
-	scene.add( object );
-	splineHelperObjects.push( object );
-	return object;
-}
-
-function addPoint() {
-	splinePointsLength ++;
-	positions.push( addSplineObject().position );
-	updateSplineOutline();
-}
-
-function removePoint() {
-	if ( splinePointsLength <= 4 ) { return; }
-	splinePointsLength --;
-	positions.pop();
-	scene.remove( splineHelperObjects.pop() );
-	updateSplineOutline();
-}
-
-function load( new_positions ) {
-	while ( new_positions.length > positions.length ) { addPoint(); }
-	while ( new_positions.length < positions.length ) { removePoint(); }
-	for ( var i = 0; i < positions.length; i ++ ) {
-		positions[ i ].copy( new_positions[ i ] );
-	}
-	updateSplineOutline();
-}
-
-function cancelHideTransorm() {
-	if ( hiding ) clearTimeout( hiding );
-}
-
-function delayHideTransform() {
-	cancelHideTransorm();
-	hideTransform();
-}
-
-function hideTransform() {
-	hiding = setTimeout( function() {
-		transformControl.detach( transformControl.object );
-	}, 2500 )
 }
 
 // Ray caster -> The mouse
